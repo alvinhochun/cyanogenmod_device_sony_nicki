0aa2a3fccd918a37ff607f50aa169062fb783fdc
Alvin Wong <alvinhochun@gmail.com>
2013-10-12 00:38:10 +0800

    Adds device-specific init.rc for recovery

--- device_nicki.mk
+++ device_nicki.mk
@@ -22,3 +22,6 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_nicki
 PRODUCT_DEVICE := nicki
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.recovery.qcom.rc:root/init.recovery.qcom.rc
