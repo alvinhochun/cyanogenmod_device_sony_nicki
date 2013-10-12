70ba7307551a00494b405b8658c63e8cd3d6ad32
Alvin Wong <alvinhochun@gmail.com>
2013-10-12 18:52:47 +0800

    Adds panic=3 to kernel cmdline to reboot on kernel panic

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 #TARGET_BOOTLOADER_BOARD_NAME := nicki
 TARGET_BOOTLOADER_BOARD_NAME := qcom
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
